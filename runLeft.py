50ea3b3d8df0e028abba7eef41a180d126128548
Lilium Wu <liliumwu@Liliums-MBP.wireless.yale.internal>
2018-12-06 20:53:52 -0500

    clean up code

--- runLeft.py
+++ runLeft.py
@@ -1,3 +1,6 @@
+# Identical to runRight but with different sleep offsets 
+# (the left sphero is the second to turn blue and reset)
+
 from multiprocessing import Process
 import sphero
 
@@ -34,12 +37,12 @@ from SpheroTeam.teamutil import readJsonFile, normalize_angle, angle_between_poi
 slowTime = 3
 normalTime = 2
 fastTime = 1
-returnTime = 0.9
+returnTime = 1
 
 fastSpeed = 140
 normalSpeed = 80
 slowSpeed = 55
-returnSpeed = 185
+returnSpeed = 183 # different from runRights' because of room tilt
 
 # Integrate camera
 
@@ -65,8 +68,6 @@ startTime = 0
 def main():
 	global startTime
 	startTime = time.time()
-	# startTime = time.time()
-	# print "-------------runRight time:", startTime
 
 	# Config management
 	PROJ_ROOT = os.pardir
@@ -74,12 +75,9 @@ def main():
 
 	# Class for managing multiple spheros
 	manager = sphero.SpheroManager()
-	# print "made manager, loading sphero roster"
 
-	# Initialize Sphero manager with addresses of local spheros
-	# manager= SpheroTeam.initialize.load_sphero_roster(manager, CONFIG)
 	manager= SpheroTeam.initialize.load_sphero_roster(manager, CONFIG)
-	# print "done loading roster, iterating through items"
+
 	# Verify that proper robots have been added
 	for name, device in manager._spheros.iteritems():
 		print "{}: {}".format(name, device.bt_addr)
@@ -88,68 +86,43 @@ def main():
 	# example: bots = [devices[4], devices[1]]
 	activeBots = ['RWR']
 
-	# print "done setting activeBots, going to connect"
 	# Connect to these robots
 	bots = SpheroTeam.initialize.connect_sphero_team(manager, activeBots)
 
-	# print "done connecting"
-
 	# All robots start with lights off to save power
 	for bot in bots:
-		# print time.time()
-		# print "hello a bot in bots, turning its light off"
 		bot.set_rgb(0,0,0)
-
-	# print time.time()
-	# print "done turning off lights, going to check team power state"
 
 	# Check team power state
 	SpheroTeam.print_team_status(bots)
 
 	# set starting positions for closed loop control
-	#time for right to calibrate
+	# time for right to calibrate
 	time.sleep(20)
 	for bot in bots:
 		bot.set_rgb(0, 0, 255)
 		time.sleep(2)
-		#print "getting starting position"
 		global startX
 		global startY
 		startX, startY = get_bot_position(bot, traceable_blue, tracker)
-		#print "got starting position"
 		print startX
 		print startY
-		#print "YEAH "
 		global offsets
 		print "calibrating"
 		offsets.append(calibrate_bot_direction(bot,  traceable_blue, [0, 0, 255], tracker, True))
-		#bot.set_rgb(0,0,0)
-	#reset bot post calibration
+	# reset bot post calibration
 	time.sleep(15)
 	bot_to_point(bots[0], offsets[0], startX, startY, TIMEOUT=400)
 	bot.set_rgb(0,0,0)
-	# time.sleep(5)
-	# time.sleep(20) # time for left to calibrate
-	print "ready to run behaviors"
-	#global offsets
-	#print "im getting global offsets now"
-	#offsets = get_team_offsets(bots, traceable_blue, TRACEABLE_COLOR)
 
 	# run behaviors
 	while (time.time() < startTime + 45):
 	 	continue
-		print "runRight time after waiting:", time.time()
-	for bot in bots:
-		with open('newBehaviorsOrderLeft.json') as f:
-			order = json.load(f)
-		runAllBehaviors(bot, order)
-		# print time.time(), "called runAllBehaviors"
-		
-		# print time.time(), "teeheeturning blue for 10"
-		bot.set_rgb(0,0,244)
-		time.sleep(10)
+	bot = bots[0]
+	with open('newBehaviorsOrderLeft.json') as f:
+		order = json.load(f)
+	runAllBehaviors(bot, order)
 
-	# print time.time(), "exiting"
 	exit()
 
 def runAllBehaviors(bot, order):
@@ -160,6 +133,7 @@ def runAllBehaviors(bot, order):
 				direction = item[key]
 			elif key == "behavior":
 				behavior = item[key]
+
 		# direction and behavior should be set
 		if direction == "left":
 			outAngle = 270
@@ -171,21 +145,17 @@ def runAllBehaviors(bot, order):
 			outAngle = 90
 			returnAngle = 260
 
-		# print time.time(), "calling single behavior", behavior
 		while (time.time() < startTime + timeFromStart):
 			continue
 		timeFromStart = timeFromStart + 28
 		runSingleBehavior(bot, behavior, outAngle, returnAngle)
 		time.sleep(2)
-		# print time.time(), "called single behavior", behavior
+
 		# call reset here
-		
-		time.sleep(8) #time for right to reset
+		time.sleep(8) # time for right to reset
 		bot.set_rgb(0,0,255)
 		# time.sleep(1)
-		print time.time(), "calling bot_to_point"
 		bot_to_point(bot, offsets[0], startX, startY, TIMEOUT=400)
-		print time.time(), "called bot_to_point"
 		bot.set_rgb(0,0,0)
 		
 
@@ -260,25 +230,17 @@ def display_current_video(tracker):
 def get_bot_position(bot, traceable_object, tracker, samples=3, debug=False):  
 	xSamples = []
 	ySamples = []
-
-#	 traceable_object.screen_size = tracker.image_size
 	 
 	for sample in range(samples):
 		image = tracker.get_video_frame()
-		#if image.any():
-			#print "yeah have an image"
-		#if traceable_object:
-			#print "great a traceable object"
+
 		timestamp = time.time()
 		
 		if sample > 0: # ignore the first sample
 			x, y = tracker._find_traceable_in_image(image, traceable_object) # side effect: adds mask to tracker  
-			#print "x:", x, ", y:", y
 			if x:
-			#	print "appending to xSamples"
 				xSamples.append(x)
 			if y:
-			#	print "appending to ySamples"
 				ySamples.append(y)
 
 			traceable_object.add_tracking(Vector2D(x, y), timestamp)
@@ -286,10 +248,7 @@ def get_bot_position(bot, traceable_object, tracker, samples=3, debug=False):
 	if debug:	  
 		display_current_view(tracker)
 	print "{} | {} ".format(xSamples, ySamples)
-	#print "xSamples", xSamples, ", len:", len(xSamples)
-	#print "ySamples", ySamples, ", len:", len(ySamples)
 
-#	 return traceable_object.pos.x, traceable_object.pos.y
 	return (sum(xSamples) / len(xSamples)), (sum(ySamples) / len(ySamples))
 
 
@@ -297,7 +256,6 @@ def calibrate_bot_direction(bot, traceable_object, traceable_color, tracker, deb
 	"""
 		Routine for making all robots line up in the same direction
 		"""
-#	 traceable_object = traceable_object_list[0]
 	TIMEOUT = 1500  # in MILLISECONDS
 	
 	
@@ -305,8 +263,6 @@ def calibrate_bot_direction(bot, traceable_object, traceable_color, tracker, deb
 	bot.set_motion_timeout(TIMEOUT)
 	startX, startY =  get_bot_position(bot, traceable_object, tracker)
 
-#	 traceable_object_list = tracker.track_object(traceable_object_list)
-#	 startX, startY = traceable_object_list[0].pos.x, traceable_object_list[0].pos.y
 	cv2.waitKey(250)  # not sure how long this has to be
 	
 	
@@ -320,22 +276,12 @@ def calibrate_bot_direction(bot, traceable_object, traceable_color, tracker, deb
 	time.sleep(TIMEOUT / 1000)
 
 	endX, endY	   =  get_bot_position(bot, traceable_object, tracker)
-#	 traceable_object_list = tracker.track_object(traceable_object_list)
-#	 endX, endY = traceable_object_list[0].pos.x, traceable_object_list[0].pos.y
 	  
 	print "End   ({},{})".format(   endX, endY)
 	offset = normalize_angle(angle_between_points(startX, startY, endX, endY))
 	
 
 	print "Angle {}".format(offset) 
-#	 bot.roll(50, offset)
-#	 time.sleep(1.5)
-#	 bot.set_heading(offset)
-	
-	
-	# Turn lights off 
-	
-#	 bot.set_rgb(0, 0, 0)
 	
 	return offset
 
@@ -347,7 +293,6 @@ def display_tracking_window(traceable_object_list, exitKey = "q"):
 	"""
 	
 	while(True):
-		#	 tracklist = tracker.track_objects(tracklist)
 
 		tracker.track_objects(tracklist)
 		cv2.waitKey(1)
@@ -372,16 +317,11 @@ def get_team_offsets(bots, traceable_object, traceable_color):
 			continue
 	return offsets
 
-#Print what the camera is currently seeing as a debugging step
-#display_current_view(tracker)
 def bot_to_point(bot, offset,
 				 targetX, targetY, 
 				 TIMEOUT=800,
 				 trace_object=traceable_blue, trace_color=TRACEABLE_COLOR, 
 				 tracker=tracker, MAX_SECONDS=10, stopRadius=5):
-	# print "in bot_to_point: printing params"
-	# print bot, offset, targetX, targetY, TIMEOUT, trace_object, trace_color, tracker, MAX_SECONDS, stopRadius
-	
 	"""
 		Currently makes use of a few implicit globals for simplicity
 		specifically traceable_blue, traceable_color
@@ -397,9 +337,7 @@ def bot_to_point(bot, offset,
 		
 		Or boost speed when within range: pause and then burst
 	"""
-	#print "in bot_to_point, calling get_bot_position with params", bot, trace_object, tracker
 	currentX, currentY = get_bot_position(bot, trace_object, tracker)
-	#print "in bot_to_point, called get_bot_position"
 
 	# Basic closed loop controller
 	startTime = time.time()
@@ -429,19 +367,15 @@ def bot_to_point(bot, offset,
 			time.sleep(0.3)
 			outSpeed = 70
 
-		# roll the sphero, make use of the request object
 		print("Dist {} outSpeed {} at {} degrees: {},{}"\
 			  .format(distance, outSpeed, angle, currentX, currentY))
 		
 		SpheroTeam.roll_sphero(bot, outSpeed, -angle, offset)
 		
-#		 bot.roll(outSpeed, angle + offset)
 		time.sleep(TIMEOUT/1000.0)
 		time.sleep(.3) # give camera time to catch up
 
-		#print "in bot_to_point, calling get_bot_position a SECOND TIME"
 		currentX , currentY = get_bot_position(bot, trace_object, tracker)
-		#print "in bot_to_point, called get_bot_position A SECOND TIME"
 
 		# Repeat waypointing calculation
 		deltaX = targetX - currentX
--- runRight.py
+++ runRight.py
@@ -1,3 +1,6 @@
+# Identical to runRight but with different sleep offsets 
+# (the right sphero is the first to turn blue and reset)
+
 from multiprocessing import Process
 import sphero
 
@@ -65,8 +68,6 @@ startTime = 0
 def main():
 	global startTime
 	startTime = time.time()
-	
-	# print "-------------runRight time:", startTime
 
 	# Config management
 	PROJ_ROOT = os.pardir
@@ -77,9 +78,8 @@ def main():
 	# print "made manager, loading sphero roster"
 
 	# Initialize Sphero manager with addresses of local spheros
-	# manager= SpheroTeam.initialize.load_sphero_roster(manager, CONFIG)
 	manager= SpheroTeam.initialize.load_sphero_roster(manager, CONFIG)
-	# print "done loading roster, iterating through items"
+
 	# Verify that proper robots have been added
 	for name, device in manager._spheros.iteritems():
 		print "{}: {}".format(name, device.bt_addr)
@@ -88,75 +88,42 @@ def main():
 	# example: bots = [devices[4], devices[1]]
 	activeBots = ['RPB']
 
-	# print "done setting activeBots, going to connect"
 	# Connect to these robots
 	bots = SpheroTeam.initialize.connect_sphero_team(manager, activeBots)
 
-	# print "done connecting"
-
 	# All robots start with lights off to save power
 	for bot in bots:
-		# print time.time()
-		# print "hello a bot in bots, turning its light off"
 		bot.set_rgb(0,0,0)
-
-	# print time.time()
-	# print "done turning off lights, going to check team power state"
 
 	# Check team power state
 	SpheroTeam.print_team_status(bots)
-	# print time.time()
-	# print "printed team status"
-
-	# outSpeed = 60
-	# outAngle = 0 
-	# outTime = 1 
-	# returnSpeed = 70
-	# returnAngle = 180
-	# returnTime = 0.8
 
 	# set starting positions for closed loop control
 	for bot in bots:
 		bot.set_rgb(0, 0, 255)
 		time.sleep(2)
-		#print "getting starting position"
 		global startX
 		global startY
 		startX, startY = get_bot_position(bot, traceable_blue, tracker)
-		#print "got starting position"
 		print startX
 		print startY
-		#print "YEAH "
 		global offsets
-		print "calibrating"
 		offsets.append(calibrate_bot_direction(bot,  traceable_blue, [0, 0, 255], tracker, True))
-		#bot.set_rgb(0,0,0)
 	#reset bot post calibration
 	time.sleep(6)
 	bot_to_point(bots[0], offsets[0], startX, startY, TIMEOUT=400)
 	bot.set_rgb(0,0,0)
-	# time.sleep(5)
-	# time.sleep(20) # time for left to calibrate
 	print "ready to run behaviors"
-	#global offsets
-	#print "im getting global offsets now"
-	#offsets = get_team_offsets(bots, traceable_blue, TRACEABLE_COLOR)
 
 	# run behaviors
 	while (time.time() < startTime + 45):
 		continue
-	# print "runRight time after waiting:", time.time()
-	for bot in bots:
-		with open('newBehaviorsOrderRight.json') as f:
-			order = json.load(f)
-		runAllBehaviors(bot, order)
-		# print time.time(), "called runAllBehaviors"
-		
-		# print time.time(), "teeheeturning blue for 10"
-		bot.set_rgb(0,0,244)
-		time.sleep(10)
+	bot = bots[0]
 
-	# print time.time(), "exiting"
+	with open('newBehaviorsOrderRight.json') as f:
+		order = json.load(f)
+	runAllBehaviors(bot, order)
+
 	exit()
 
 def runAllBehaviors(bot, order):
@@ -167,6 +134,7 @@ def runAllBehaviors(bot, order):
 				direction = item[key]
 			elif key == "behavior":
 				behavior = item[key]
+
 		# direction and behavior should be set
 		if direction == "left":
 			outAngle = 270
@@ -178,20 +146,16 @@ def runAllBehaviors(bot, order):
 			outAngle = 90
 			returnAngle = 260
 
-		# print time.time(), "calling single behavior", behavior	
 		while (time.time() < startTime + timeFromStart):
 			continue
 		timeFromStart = timeFromStart + 28
 		runSingleBehavior(bot, behavior, outAngle, returnAngle)
 		time.sleep(2)
-		# print time.time(), "called single behavior", behavior
 		
 		# call reset here
 		bot.set_rgb(0,0,255)
 		time.sleep(1)
-		print time.time(), "calling bot_to_point"
 		bot_to_point(bot, offsets[0], startX, startY, TIMEOUT=400)
-		print time.time(), "called bot_to_point"
 		bot.set_rgb(0,0,0)
 		time.sleep(8) # left resets next
 		
@@ -206,30 +170,16 @@ def runSingleBehavior(bot, behavior, outAngle, returnAngle):
 			goBlink(bot, normalSpeed, outAngle, normalTime, returnSpeed, returnAngle, returnTime)
 		elif behavior == "solid_light":
 			goSolidLight(bot, normalSpeed, outAngle, normalTime, returnSpeed, returnAngle, returnTime)
-		# goAtSpeed(bot, normalSpeed, outAngle, normalTime, returnSpeed, returnAngle, returnTime)
-	# print time.time(), "called behavior"
 	time.sleep(0.25)
-	bot.set_rgb(0,0,00)
+	bot.set_rgb(0,0,0)
 	bot.set_motion_timeout(returnTime * 1000)
 	bot.roll(returnSpeed, returnAngle, 1)
 	time.sleep(returnTime)
 
 def goAtSpeed(bot, outSpeed, outAngle, outTime, returnSpeed, returnAngle, returnTime):
-	# print time.time(), "in goAtSpeed, speed is", outSpeed
 	bot.set_motion_timeout(outTime * 1000)
 	bot.roll(outSpeed, outAngle, 1)
-	# time.sleep(1)
-	# bots[0].stop()
 	time.sleep(6)
-
-	# bot.roll(0, outAngle, 0)
-	# time.sleep(0.5)
-	# bot.roll(outSpeed, outAngle, 1)
-	# time.sleep(outTime)
-	# bot.roll(0,0,0)
-	# time.sleep(6 - 0.5 - outTime)
-
-	# print time.time(), "end of goAtSpeed"
 
 def goBlink(bot, outSpeed, outAngle, outTime, returnSpeed, returnAngle, returnTime):
 	bot.set_motion_timeout(outTime * 1000)
@@ -251,26 +201,7 @@ def goSolidLight(bot, outSpeed, outAngle, outTime, returnSpeed, returnAngle, ret
 	bot.set_motion_timeout(outTime * 1000)
 	bot.roll(outSpeed, outAngle, 1)
 	time.sleep(6 - outTime)
-	# bot.set_rgb(0,255,0)
 
-
-# def startBlink(bot, duration):
-# 	on = True
-# 	for quarter_second in range(0, 4 * duration):
-# 		if on:
-# 			bot.set_rgb(0,0,0)
-# 			on = False
-# 		else:
-# 			bot.set_rgb(138,191,255)
-# 			on = True
-# 		time.sleep(0.25)
-
-# 	bot.set_rgb(0,255,0)
-
-# def startSolidLight(bot, time):
-# 	bot.set_rgb(138,191,255)
-# 	time.sleep(time)
-# 	bot.set_rgb(0,255,0)
 
 # For Debugging
 def display_current_view(tracker):
@@ -302,25 +233,16 @@ def display_current_video(tracker):
 def get_bot_position(bot, traceable_object, tracker, samples=3, debug=False):  
 	xSamples = []
 	ySamples = []
-
-#	 traceable_object.screen_size = tracker.image_size
 	 
 	for sample in range(samples):
 		image = tracker.get_video_frame()
-		#if image.any():
-			#print "yeah have an image"
-		#if traceable_object:
-			#print "great a traceable object"
 		timestamp = time.time()
 		
 		if sample > 0: # ignore the first sample
 			x, y = tracker._find_traceable_in_image(image, traceable_object) # side effect: adds mask to tracker  
-			#print "x:", x, ", y:", y
 			if x:
-			#	print "appending to xSamples"
 				xSamples.append(x)
 			if y:
-			#	print "appending to ySamples"
 				ySamples.append(y)
 
 			traceable_object.add_tracking(Vector2D(x, y), timestamp)
@@ -328,10 +250,7 @@ def get_bot_position(bot, traceable_object, tracker, samples=3, debug=False):
 	if debug:	  
 		display_current_view(tracker)
 	print "{} | {} ".format(xSamples, ySamples)
-	#print "xSamples", xSamples, ", len:", len(xSamples)
-	#print "ySamples", ySamples, ", len:", len(ySamples)
 
-#	 return traceable_object.pos.x, traceable_object.pos.y
 	return (sum(xSamples) / len(xSamples)), (sum(ySamples) / len(ySamples))
 
 
@@ -347,8 +266,6 @@ def calibrate_bot_direction(bot, traceable_object, traceable_color, tracker, deb
 	bot.set_motion_timeout(TIMEOUT)
 	startX, startY =  get_bot_position(bot, traceable_object, tracker)
 
-#	 traceable_object_list = tracker.track_object(traceable_object_list)
-#	 startX, startY = traceable_object_list[0].pos.x, traceable_object_list[0].pos.y
 	cv2.waitKey(250)  # not sure how long this has to be
 	
 	
@@ -362,22 +279,12 @@ def calibrate_bot_direction(bot, traceable_object, traceable_color, tracker, deb
 	time.sleep(TIMEOUT / 1000)
 
 	endX, endY	   =  get_bot_position(bot, traceable_object, tracker)
-#	 traceable_object_list = tracker.track_object(traceable_object_list)
-#	 endX, endY = traceable_object_list[0].pos.x, traceable_object_list[0].pos.y
 	  
 	print "End   ({},{})".format(   endX, endY)
 	offset = normalize_angle(angle_between_points(startX, startY, endX, endY))
 	
 
 	print "Angle {}".format(offset) 
-#	 bot.roll(50, offset)
-#	 time.sleep(1.5)
-#	 bot.set_heading(offset)
-	
-	
-	# Turn lights off 
-	
-#	 bot.set_rgb(0, 0, 0)
 	
 	return offset
 
@@ -389,7 +296,6 @@ def display_tracking_window(traceable_object_list, exitKey = "q"):
 	"""
 	
 	while(True):
-		#	 tracklist = tracker.track_objects(tracklist)
 
 		tracker.track_objects(tracklist)
 		cv2.waitKey(1)
@@ -415,15 +321,11 @@ def get_team_offsets(bots, traceable_object, traceable_color):
 	return offsets
 
 #Print what the camera is currently seeing as a debugging step
-#display_current_view(tracker)
 def bot_to_point(bot, offset,
 				 targetX, targetY, 
 				 TIMEOUT=800,
 				 trace_object=traceable_blue, trace_color=TRACEABLE_COLOR, 
-				 tracker=tracker, MAX_SECONDS=10, stopRadius=5):
-	# print "in bot_to_point: printing params"
-	# print bot, offset, targetX, targetY, TIMEOUT, trace_object, trace_color, tracker, MAX_SECONDS, stopRadius
-	
+				 tracker=tracker, MAX_SECONDS=10, stopRadius=5):	
 	"""
 		Currently makes use of a few implicit globals for simplicity
 		specifically traceable_blue, traceable_color
@@ -439,9 +341,7 @@ def bot_to_point(bot, offset,
 		
 		Or boost speed when within range: pause and then burst
 	"""
-	#print "in bot_to_point, calling get_bot_position with params", bot, trace_object, tracker
 	currentX, currentY = get_bot_position(bot, trace_object, tracker)
-	#print "in bot_to_point, called get_bot_position"
 
 	# Basic closed loop controller
 	startTime = time.time()
@@ -477,13 +377,10 @@ def bot_to_point(bot, offset,
 		
 		SpheroTeam.roll_sphero(bot, outSpeed, -angle, offset)
 		
-#		 bot.roll(outSpeed, angle + offset)
 		time.sleep(TIMEOUT/1000.0)
 		time.sleep(.3) # give camera time to catch up
 
-		#print "in bot_to_point, calling get_bot_position a SECOND TIME"
 		currentX , currentY = get_bot_position(bot, trace_object, tracker)
-		#print "in bot_to_point, called get_bot_position A SECOND TIME"
 
 		# Repeat waypointing calculation
 		deltaX = targetX - currentX
